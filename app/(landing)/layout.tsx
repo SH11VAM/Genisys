363dfdc4d69a3820d5199e34d0e4b852323d688f
Shivam <jainshivam2598@gmail.com>
2024-06-09 00:51:39 +0530

    Update Next js

--- app/(landing)/layout.tsx
+++ app/(landing)/layout.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const LandingLayout = ({children} : {children : React.ReactNode}) => {
     return (
-        <main className="h-full bg-[#020617] font-mono overflow-auto">
+        <main className="h-full bg-black  font-mono overflow-auto">
             <div className="mx-auto max-w-screen-xl h-full w-full">
                 {children}
             </div>
--- components/landing-hero.tsx
+++ components/landing-hero.tsx
@@ -12,8 +12,15 @@ export const LandingHero = () => {
   return (
     <div className="text-white font-bold py-36 text-center space-y-5">
       <div className="text-4xl sm:text-5xl md:text-6xl lg:text-7xl space-y-5 font-extrabold">
-        Unlock Infinite Creativity With <br /> Our Generative AI <br /> Genisys
-    
+        <div className="h-[50rem] w-full bg-black  bg-grid-white/[0.2]  relative flex items-center justify-center">
+          {/* Radial gradient for the container to give a faded look */}
+          <div className="absolute pointer-events-none inset-0 flex items-center justify-center bg-black  [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
+          <p className="text-4xl sm:text-7xl font-bold relative z-20 bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-500 py-8">
+            Unlock Infinite Creativity With <br /> Our Generative AI <br />
+            Genisys
+          </p>
+        </div>
+
         <div className="text-transparent bg-clip-text bg-gradient-to-r from-indigo-200 via-sky-300 to-blue-700 p-2">
           <TypewriterComponent
             options={{
@@ -21,7 +28,7 @@ export const LandingHero = () => {
                 "Chatbot.",
                 "Photo Generation.",
                 "Blog Writing.",
-                "Mail Writing."
+                "Mail Writing.",
               ],
               autoStart: true,
               loop: true,
@@ -34,7 +41,10 @@ export const LandingHero = () => {
       </div>
       <div>
         <Link href={isSignedIn ? "/dashboard" : "/sign-up"}>
-          <Button variant="secondary" className="md:text-lg p-4 md:p-6 rounded-full font-semibold bg-gradient-to-r from-sky-400 to-blue-500 hover:from-pink-500 text-white hover:to-yellow-500 hover:text-slate-800 hover:font-extrabold">
+          <Button
+            variant="secondary"
+            className="md:text-lg p-4 md:p-6 rounded-full font-semibold bg-gradient-to-r from-sky-400 to-blue-500 hover:from-pink-500 text-white hover:to-yellow-500 hover:text-slate-800 hover:font-extrabold"
+          >
             Start Generating For Free
           </Button>
         </Link>
