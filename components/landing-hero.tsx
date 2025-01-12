2e1329d01547dff650b779e269fb5d9553e32b03
SH11VAM <jainshivam12527@gmail.com>
2025-01-12 23:45:33 +0530

    .

--- components/landing-hero.tsx
+++ components/landing-hero.tsx
@@ -4,12 +4,19 @@ import TypewriterComponent from "typewriter-effect";
 import Link from "next/link";
 import { useAuth } from "@clerk/nextjs";
 import { Button } from "@/components/ui/button";
+import { motion } from "framer-motion";
 
 export const LandingHero = () => {
   const { isSignedIn } = useAuth();
 
   return (
-    <div className="text-white font-bold py-36 text-center space-y-5 grid place-items-center">
+    <div className="text-white font-bold py-36 text-center space-y-5 ">
+       <motion.div
+            initial={{ opacity: 0, y: 20 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 1.2 }}
+            className="text-center"
+          >
       <div className="mb-5">
         <p
           className={` text-4xl sm:text-7xl font-extrabold font-lota relative z-20 bg-clip-text text-[#FEFEFF] py-8`}
@@ -17,6 +24,7 @@ export const LandingHero = () => {
           Empowering Your Ideas With Generative AI Excellence.
         </p>
       </div>
+      </motion.div>
 
       <div className="text-[#FEFEFF] p-2 font-lota">
         <TypewriterComponent
@@ -34,8 +42,15 @@ export const LandingHero = () => {
         />
       </div>
 
+      <motion.div
+            initial={{ opacity: 0, y: 20 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 1.3 }}
+            
+          >
+
       <div
-        className={`text-lg md:text-xl  text-zinc-400 font-extrabold font-lota `}
+        className={`text-lg md:text-xl  text-zinc-400 font-extrabold font-lota mb-4`}
       >
         Create content using AI 10x faster.
       </div>
@@ -43,15 +58,16 @@ export const LandingHero = () => {
         <Link href={isSignedIn ? "/dashboard" : "/sign-up"}>
           <Button
             variant="secondary"
-            className={`  md:text-lg sm:text-sm lg:text-xl p-3 md:p-4 lg:p-6 rounded-xl font-lota  text-black bg-white bg-gradient-to-br  hover:from-[#C53678] hover:via-[#F79C1A] hover:to-[#7C408A] hover:text-black hover:font-extrabold `}
+            className={`  md:text-lg sm:text-sm lg:text-xl p-3 md:p-4 lg:p-6 rounded-xl font-lota mb-4 text-black bg-white bg-gradient-to-br  hover:from-[#C53678] hover:via-[#F79C1A] hover:to-[#7C408A] hover:text-black hover:font-extrabold `}
           >
             Start Generating For Free
           </Button>
         </Link>
       </div>
-      <div className={`text-zinc-400 text-lg md:text-sm font-bold  font-lota`}>
+      <div className={`text-zinc-400 text-lg md:text-sm font-bold  font-lota `}>
         No credit card required.
       </div>
+      </motion.div>
     </div>
   );
 };
