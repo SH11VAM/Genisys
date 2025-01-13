b5d57478b10030c8d5e7e9bb2778e27b58cf6ff1
SH11VAM <jainshivam12527@gmail.com>
2025-01-13 14:03:31 +0530

    Commit

--- components/landing-hero.tsx
+++ components/landing-hero.tsx
@@ -45,7 +45,7 @@ export const LandingHero = () => {
       <motion.div
             initial={{ opacity: 0, y: 20 }}
             animate={{ opacity: 1, y: 0 }}
-            transition={{ duration: 1.3 }}
+            transition={{ duration: 0.4 }}
             
           >
 
